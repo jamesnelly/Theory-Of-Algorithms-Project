eeb2a5d6ec10d458a52a97ff68b3394a4c72d57c
jamesnelly <g00346996@gmit.ie>
2020-04-05 22:50:51 +0100

    updated md5 added more functions

--- MD5_/md5.c
+++ MD5_/md5.c
@@ -6,11 +6,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 
 #define GREEN  "\x1B[32m"
 #define NORMAL "\x1B[0m"
 
+// prints out some bits
 void print_64_bits(unsigned long b)
 {
     int i;
@@ -23,6 +26,7 @@ void print_64_bits(unsigned long b)
     putchar('\n');
 }
 
+// prints out some bits
 void print_32_bits(int b)
 {
     int i;
@@ -53,6 +57,7 @@ void print_int_array_hex(unsigned int b[], int bufferLength)
     putchar('\n');
 }
 
+// This specifies the per-round shift amounts
 int shifts[64] = {
     7, 12, 17, 22,  7, 12, 17, 22,  
     7, 12, 17, 22,  7, 12, 17, 22,
@@ -64,7 +69,28 @@ int shifts[64] = {
     6, 10, 15, 21,  6, 10, 15, 21
 };
 
-int main(){
+// Initialize Varibles
+int a = 0x67452301; //A
+int b = 0xefcdab89; //B
+int c = 0x98badcfe; //C
+int d = 0x10325476; //D
 
+unsigned int K[64];
+
+void print_header(char *message)
+{
+    printf("\n%s# %s%s\n", GREEN, message, NORMAL);
+}
+
+// Generating K
+void generate_k()
+{
+    // computing K
+    for(int i=0;i<64;i++) {
+        K[i] = (unsigned int)(fabs(sin(i + 1)) * (1L << 32));
+    }
+}
+int main(){
+    
 }
 
