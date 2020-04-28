826f4d78de51776aae8e920e99cc52cb470e723a
jamesnelly <g00346996@gmit.ie>
2020-04-28 12:13:54 +0100

    added command line feature --input where you can input a string to hash added comments to test()

--- MD5_/md5.c
+++ MD5_/md5.c
@@ -118,9 +118,10 @@ unsigned int rotate_left(unsigned int temp, int times)
     return (temp << times) | (temp >> (32 - times));
 }
 
+// void methods
 static void test(){
     size_t i = 0;
-   unsigned char message [5] [10] = {
+   unsigned char message [5] [12] = {
        "hello",
        "james",
        "ireland",
@@ -129,21 +130,26 @@ static void test(){
        };
 
     for (i = 0; i < sizeof(message) / sizeof(message[0]); i++){
-        printf("\nstring =  %s    ", message + i);
-
+        printf("\n=============================================================");
+        printf("\n            string %d =   %s    ",i, message + i);
+        printf("\n");
+     
         unsigned long bitlength = strlen((char*)message[i]) * sizeof((char*)message[i]);
-        message[i][strlen((char*)message)] = 0x80; 
-        memcpy(message + 56, &bitlength, 8);    
+        // adding 1 bit of padding
+        message[i][strlen((char*)message[i + 1])] = 0x80; 
+        // appending bit length at the end
+        memcpy(message[i] + 56, &bitlength, 8);    
         generate_k();
+         // break chunk into sixteen 32-bit words M[j], 0 ≤ j ≤ 15
         unsigned int chunks[16];
-        memcpy(chunks, &message, 64);
+        memcpy(chunks, &message[i], 64);
         int A = a;
         int B = b;
         int C = c;
         int D = d;
         int prev_round = 0;
 
-            for (int i=0; i<64; i++) {
+        for (int i=0; i<64; i++) {
         int k = i;
         int function_result = 0;
         int s = shifts[i];
@@ -182,55 +188,28 @@ static void test(){
     unsigned char md5[16];
     memcpy(md5, md, 16);
 
-   // print_header("MD5 Hash");
+    print_header("MD5 Hash");
     print_hex(md5, 16);
-
+     printf("\n=============================================================");
    }
-
-
 }
 
-int main(int argc, char* argv[]){
-
-    int i ;
-
-    if( argc == 2){
-         printf("The arguments are --help --test --input :\n");
-         for(i = 1; i < argc; i++){
-             printf("%s\t", argv[i]);
-             if(strcmp(argv[i],"--help") == 0)
-             {   printf("\n ================================================================================================");
-                 printf("\n =     This is the MD5 Algorithim i have designed for the assignment                            =");
-                 printf("\n =     I have setup the Algorthim in a way in which you can input the string you would like     =");
-                 printf("\n =     The Algorithm will then HASH the input and output the Hashed version                     =");
-                printf("\n =================================================================================================");
-                }else if (strcmp(argv[i], "--test") == 0){
-                        test();
-                }else if (strcmp(argv[i], "--input") == 0){
-                    printf("\nInput your own text");
-                }else{
-                    printf("no command exists");
-                }
-            }
-        }
-   
-
-    unsigned char message[64] = "Hello World";
+static void input()
+{
+    // variable for the string to be assigned too
+    unsigned char message[64];
 
     // Message prompting to input a string to Hash
-    //printf("enter a string to hash \n");
-      // Input the String
-    //scanf("%s", message);
+    printf("\n Enter a String to hash \n");
+    // Input the String
+    scanf("%s", message);
 
     unsigned long bitlength = strlen((char*)message) * sizeof((char*)message);
 
-  //  printf("Message(%lu) = %s\n", strlen((char*)message), message);
-
-   // adding 1 bit of padding
-   message[strlen((char*)message)] = 0x80; 
-   // appending bit length at the end
-    memcpy(message + 56, &bitlength, 8);    
-    //print_hex(message, 64);
+    // adding 1 bit of padding
+    message[strlen((char*)message)] = 0x80; 
+    // appending bit length at the end
+    memcpy(message + 56, &bitlength, 8);
 
     generate_k();
      // break chunk into sixteen 32-bit words M[j], 0 ≤ j ≤ 15
@@ -242,19 +221,18 @@ int main(int argc, char* argv[]){
     int C = c;
     int D = d;
 
-    int prev_round = 0;
+    int prev_round = 0; 
 
+    
     for (int i=0; i<64; i++) {
-        int k = i;
-        int function_result = 0;
-        int s = shifts[i];
-        int round = (i >> 4) + 1;
+            int k = i;
+            int function_result = 0;
+            int s = shifts[i];
+            int round = (i >> 4) + 1;
 
         if (round != prev_round) {
             prev_round = round;
-            //print_header("New round applying new filters");
         }
-
         if (round == 1)
             function_result = f(B, C, D);
         else if (round == 2) {
@@ -279,17 +257,41 @@ int main(int argc, char* argv[]){
 
         int debug[4] = {A, B, C, D};
 
-       // printf("\tA=%02X B=%02X C=%02X D=%02X\n", debug[(i + 1) & 3], debug[(i + 2) & 3], debug[(i + 3) & 3], debug[i & 3]);
     }
+        int md[4] = {A + a, B + b, C + c, D + d};
 
-    int md[4] = {A + a, B + b, C + c, D + d};
+        unsigned char md5[16];
+        memcpy(md5, md, 16);
 
-    unsigned char md5[16];
-    //memcpy(md5, md, 16);
+        print_header("MD5 Hash");
+        print_hex(md5, 16);  
+}
 
-    //print_header("MD5 Hash");
-    //print_hex(md5, 16);
-    //system("pause");
-    return 0;
+
+int main(int argc, char* argv[]){
+
+    int i ;
+
+    if( argc == 2){
+         printf("The arguments are --help --test --input :\n");
+         for(i = 1; i < argc; i++){
+             printf("%s\t", argv[i]);
+             if(strcmp(argv[i],"--help") == 0)
+             {   printf("\n ================================================================================================");
+                 printf("\n =     This is the MD5 Algorithim i have designed for the assignment                            =");
+                 printf("\n =     I have setup the Algorthim in a way in which you can input the string you would like     =");
+                 printf("\n =     The Algorithm will then HASH the input and output the Hashed version                     =");
+                printf("\n =================================================================================================");
+                }else if (strcmp(argv[i], "--test") == 0){
+                        test();
+                }else if (strcmp(argv[i], "--input") == 0){
+                        input();
+                }else{
+                    printf("no command exists");
+                }
+            }
+            
+        } 
+        return 0;
 }
 
