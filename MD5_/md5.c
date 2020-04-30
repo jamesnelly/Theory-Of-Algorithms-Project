6a9d6569465c3538ea7b055aef4ef8b135a8014e
jamesnelly <g00346996@gmit.ie>
2020-04-30 21:56:14 +0100

    added comments to the code deleted some not needed anymore added the Research in PDF format

--- MD5_/md5.c
+++ MD5_/md5.c
@@ -13,45 +13,11 @@
 #define GREEN  "\x1B[32m"
 #define NORMAL "\x1B[0m"
 
-// prints out some bits
-void print_64_bits(unsigned long b)
-{
-    int i;
-    int s = 8 * (sizeof b) - 1;
-
-    for (i = s; i >= 0; i--) {
-        unsigned long mask = 1L << i;
-        putchar(b & mask ? '1' : '0');
-    }
-    putchar('\n');
-}
-
-// prints out some bits
-void print_32_bits(int b)
-{
-    int i;
-    int s = 8 * (sizeof b) - 1;
-
-    for (i = s; i >= 0; i--) {
-        int mask = 1 << i;
-        putchar(b & mask ? '1' : '0');
-    }
-    putchar('\n');
-}
-
+// prints the hash of the MD5 result.
 void print_hex(unsigned char b[], int bufferLength)
 {
     for(int i=0;i<bufferLength;i++) {
         printf("%02X ", b[i]);
-    }
-
-    putchar('\n');
-}
-
-void print_int_array_hex(unsigned int b[], int bufferLength)
-{
-    for(int i=0;i<bufferLength;i++) {
-        printf("0x%02x\t", b[i]);
     }
 
     putchar('\n');
@@ -75,13 +41,12 @@ int b = 0xefcdab89; //B
 int c = 0x98badcfe; //C
 int d = 0x10325476; //D
 
-unsigned int K[64];
-
 void print_header(char *message)
 {
     printf("\n%s# %s%s\n", GREEN, message, NORMAL);
 }
-
+// variable for K
+unsigned int K[64];
 // Generating K
 void generate_k()
 {
@@ -155,6 +120,7 @@ static void test(){
         int s = shifts[i];
         int round = (i >> 4) + 1;
 
+        // Rounds being performed
         if (round != prev_round) {
             prev_round = round;
         }
